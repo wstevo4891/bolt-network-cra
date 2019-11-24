c75416cbd34d2eb8e0042e2f27c76b053c954ebe
Eric Stephenson <ericb.stephenson@gmail.com>
2019-11-24 15:31:05 -0800

    Add moviesIndexReducer test

--- src/store/__tests__/moviesIndexActions.test.js
+++ src/store/__tests__/moviesIndexActions.test.js
@@ -9,42 +9,52 @@ const middlewares = [thunk]
 const mockStore = configureMockStore(middlewares)
 
 describe('moviesIndexActions', () => {
-  it('should create action to begin fetch', () => {
-    const expectedAction = {
-      type: types.FETCH_MOVIES_INDEX_BEGIN
-    }
+  const PAYLOAD = {
+    Action: [{ title: 'The Avengers' }],
+    'Sci-Fi': [{ title: 'Alien' }]
+  }
 
-    expect(actions.fetchMoviesIndexBegin()).toEqual(expectedAction)
+  const moviesIndexError = new Error('Failure in fetchMoviesIndex()')
+
+  const beginAction = {
+    type: types.FETCH_MOVIES_INDEX_BEGIN
+  }
+
+  const successAction = {
+    type: types.FETCH_MOVIES_INDEX_SUCCESS,
+    payload: { moviesIndex: PAYLOAD }
+  }
+
+  const failureAction = {
+    type: types.FETCH_MOVIES_INDEX_FAILURE,
+    payload: { error: moviesIndexError }
+  }
+
+  it('should create an action to begin fetch', () => {
+    expect(actions.fetchMoviesIndexBegin()).toEqual(beginAction)
+  })
+
+  it('should create an action for a successful fetch', () => {
+    expect(
+      actions.fetchMoviesIndexSuccess(PAYLOAD)
+    ).toEqual(successAction)
+  })
+
+  it('should create an action for a failed fetch', () => {
+    expect(
+      actions.fetchMoviesIndexFailure(moviesIndexError)
+    ).toEqual(failureAction)
   })
 
   describe('async actions', () => {
     afterEach(() => fetchMock.restore())
 
     it('creates FETCH_MOVIES_INDEX_SUCCESS when fetch is done', () => {
-      fetchMock.getOnce(actions.URI, {
-        headers: { 'content-type': 'application/json' }
-      })
-
-      const PAYLOAD = {
-        moviesIndex: {
-          moviesIndex: {
-            Action: [{ title: 'The Avengers' }],
-            'Sci-Fi': [{ title: 'Alien' }]
-          },
-          genres: ['Action', 'Sci-Fi'],
-          genresIndex: {
-            action: { text: 'Action', url: '/genres/action' },
-            'sci-fi': { text: 'Sci-Fi', url: '/genres/sci-fi' }
-          }
-        }
-      }
+      fetchMock.getOnce(actions.URI, PAYLOAD)
 
       const expectedActions = [
-        { type: types.FETCH_MOVIES_INDEX_BEGIN },
-        {
-          type: types.FETCH_MOVIES_INDEX_SUCCESS,
-          payload: PAYLOAD
-        }
+        beginAction,
+        successAction
       ]
 
       const store = mockStore({
@@ -59,6 +69,33 @@ describe('moviesIndexActions', () => {
         expect(store.getActions()).toEqual(expectedActions)
       })
     })
+
+    // it('creates FETCH_MOVIES_INDEX_FAILURE when fetch throws an error', () => {
+    //   fetchMock.getOnce(actions.URI, {
+    //     status: 400,
+    //     body: JSON.stringify(moviesIndexError)
+    //   })
+
+    //   const expectedActions = [
+    //     { type: types.FETCH_MOVIES_INDEX_BEGIN },
+    //     {
+    //       type: types.FETCH_MOVIES_INDEX_FAILURE,
+    //       payload: { error: moviesIndexError }
+    //     }
+    //   ]
+
+    //   const store = mockStore({
+    //     moviesIndex: {
+    //       moviesIndex: {},
+    //       genres: [],
+    //       genresIndex: {}
+    //     }
+    //   })
+
+    //   return store.dispatch(actions.fetchMoviesIndex()).then(() => {
+    //     expect(store.getActions()).toEqual(expectedActions)
+    //   })
+    // })
   })
 })
 
