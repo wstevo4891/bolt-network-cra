9daa2201e29331973c74c43d23afe9c6382d2c77
Eric Stephenson <ericb.stephenson@gmail.com>
2019-11-24 10:15:46 -0800

    Install enzyme packages

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+import Store from './store'
+import App from './App'
+
+describe('App', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div')
+
+    ReactDOM.render(
+      <Provider store={Store}>
+        <App />
+      </Provider>,
+      div
+    )
+
+    ReactDOM.unmountComponentAtNode(div)
+  })
+})
